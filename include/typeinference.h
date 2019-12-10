0b932dc135e8ef42d5666ed61f54589e1ff4981e
Jake Fecher <jfecher11@gmail.com>
2019-12-10 00:02:37 -0500

    Fix typo

--- include/typeinference.h
+++ include/typeinference.h
@@ -49,7 +49,7 @@ namespace ante {
             std::cout << "Module: " << module->name << '\n';
             std::cout << "Type inference: " << duration_cast<milliseconds>(end - tistart).count() << "ms\n";
             std::cout << "    Initialization:     " << duration_cast<milliseconds>(cfstart - tistart).count() << "ms\n";
-            std::cout << "    Constraing finding: " << duration_cast<milliseconds>(unifystart - cfstart).count() << "ms\n";
+            std::cout << "    Constraint finding: " << duration_cast<milliseconds>(unifystart - cfstart).count() << "ms\n";
             std::cout << "    Unification:        " << duration_cast<milliseconds>(substart - unifystart).count() << "ms\n";
             std::cout << "    Substitution:       " << duration_cast<milliseconds>(end - substart).count() << "ms\n";
         }
