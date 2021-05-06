93bf99950bdff98e197e80fd13f4d5f05d0e46f2
Ahmad Sattar <thehabbos007@gmail.com>
2021-05-06 19:46:28 +0200

    Change fmap to used generic sized intoiterator (#79)

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -22,7 +22,7 @@ mod error;
 pub mod ast;
 pub mod pretty_printer;
 
-use crate::{error::location::Locatable, lexer::token::Token};
+use crate::{error::location::Locatable, lexer::token::Token, util::fmap};
 use ast::{ Ast, Type, Trait, TypeDefinitionBody };
 use error::{ ParseError, ParseResult };
 use crate::error::location::Location;
@@ -486,19 +486,16 @@ fn desugar_explicit_currying<'a>(function: Ast<'a>, args: Vec<Ast<'a>>, loc: Loc
 fn curried_function_call<'a>(function: Ast<'a>, args: Vec<Ast<'a>>, loc: Location<'a>) -> Ast<'a> {
     let mut curried_args = vec![];
     let mut curried_arg_count = 0;
-    let args: Vec<Ast<'a>> = args
-        .into_iter()
-        .map(|arg| {
-            if matches_underscore(&arg) {
-                curried_arg_count += 1;
-                let curried_arg = format!("${}", curried_arg_count);
-                curried_args.push(Ast::variable(curried_arg.clone(), arg.locate()));
-                Ast::variable(curried_arg, arg.locate())
-            } else {
-                arg
-            }
-        })
-        .collect();
+    let args: Vec<Ast<'a>> = fmap(args, |arg| {
+        if matches_underscore(&arg) {
+            curried_arg_count += 1;
+            let curried_arg = format!("${}", curried_arg_count);
+            curried_args.push(Ast::variable(curried_arg.clone(), arg.locate()));
+            Ast::variable(curried_arg, arg.locate())
+        } else {
+            arg
+        }
+    });
 
     let function_call = Ast::function_call(function, args, loc);
     Ast::lambda(curried_args, None, function_call, loc)
