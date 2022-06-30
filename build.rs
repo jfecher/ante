fd1a5afa60286ff7ff3d50d8c3fc00198254110c
Shunsuke Shibayama <sbym1346@gmail.com>
2022-06-30 13:02:49 +0900

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 use std::env;
 
 fn main() -> std::io::Result<()> {
-    let cur_dir = env::current_dir()?;
-    println!("cargo:rustc-env=ANTE_STDLIB_DIR={}/stdlib", cur_dir.to_str().unwrap());
+    if option_env!("ANTE_STDLIB_DIR").is_none() {
+        let cur_dir = env::current_dir()?;
+        println!("cargo:rustc-env=ANTE_STDLIB_DIR={}/stdlib", cur_dir.to_str().unwrap());
+    }
     Ok(())
 }
