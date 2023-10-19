ffba97f9b40306586b9ba43a86a331a6a86132f2
ehllie <me@ehllie.xyz>
2023-10-19 17:07:35 +0200

    Switch to flake-parts and nci (#167)

---
+++ crates.nix
@@ -0,0 +1,52 @@
+{ ... }: {
+  perSystem = { pkgs, lib, ... }: {
+    nci =
+      let
+        llvmPackages = pkgs.llvmPackages_16;
+        major = lib.versions.major llvmPackages.llvm.version;
+        minor = lib.versions.minor llvmPackages.llvm.version;
+        llvm-sys-ver = "${major}${builtins.substring 0 1 minor}";
+        env = {
+          "LLVM_SYS_${llvm-sys-ver}_PREFIX" = llvmPackages.llvm.dev;
+        };
+      in
+      {
+        projects.ante.path = ./.;
+        crates.ante = {
+          depsDrvConfig = {
+            inherit env;
+          };
+          drvConfig = {
+            inherit env;
+            mkDerivation = {
+              nativeBuildInputs = [ pkgs.installShellFiles ];
+              buildInputs = lib.attrValues
+                {
+                  inherit (pkgs)
+                    libffi
+                    libxml2
+                    ncurses;
+                } ++ [ llvmPackages.llvm ];
+
+              postPatch = ''
+                substituteInPlace tests/golden_tests.rs --replace \
+                  'target/debug' "target/$(rustc -vV | sed -n 's|host: ||p')/release"
+              '';
+
+              preBuild = ''
+                export ANTE_STDLIB_DIR=$out/lib
+                find stdlib -type f -exec install -Dm644 "{}" -t $ANTE_STDLIB_DIR \;
+              '';
+
+              postInstall = ''
+                installShellCompletion --cmd ante \
+                  --bash <($out/bin/ante --shell-completion bash) \
+                  --fish <($out/bin/ante --shell-completion fish) \
+                  --zsh <($out/bin/ante --shell-completion zsh)
+              '';
+            };
+          };
+        };
+      };
+  };
+}
--- flake.nix
+++ flake.nix
@@ -1,35 +1,42 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nci = {
+      url = "github:yusdacra/nix-cargo-integration";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    {
-      overlays.default = _: prev:
-        { ante = (import ./.) { pkgs = prev; }; };
-    } //
-    (flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays.default ];
+  outputs = inputs@{ parts, nci, ... }:
+    parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      imports = [
+        parts.flakeModules.easyOverlay
+        nci.flakeModule
+        ./crates.nix
+      ];
+
+      perSystem = { config, ... }:
+        let crateOutputs = config.nci.outputs.ante; in
+        {
+          overlayAttrs.ante = config.packages.default;
+          packages.default = crateOutputs.packages.release;
+          devShells.default = crateOutputs.devShell.overrideAttrs (_: {
+            shellHook = ''
+              PATH=$PATH:$PWD/target/debug
+            '';
+          });
         };
-        inherit (pkgs)
-          ante mkShell rust-analyzer clippy rustfmt;
-      in
-      {
-        packages = {
-          inherit ante;
-          default = ante;
-        };
-        devShells.default = mkShell {
-          name = "ante-dev";
-          inputsFrom = [ ante ];
-          packages = [ rust-analyzer clippy rustfmt ];
-          shellHook = ante.shellHook + ''
-            export PATH=$PWD/target/debug:$PATH
-          '';
-        };
-      }));
+    };
 }
