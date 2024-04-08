27c22aa5104796889fef683458f5e100ff6ddf9c
anoojpatel <1996anoojpatel@gmail.com>
2024-04-08 00:48:08 -0400

    fix

--- src/parser/pretty_printer.rs
+++ src/parser/pretty_printer.rs
@@ -77,7 +77,7 @@ impl<'a> Display for ast::If<'a> {
 
 impl<'a> Display for ast::Else<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "( {} else {})", self.lhs, self.rhs)
+        write!(f, "({} else {})", self.lhs, self.rhs)
     }
 }
 
