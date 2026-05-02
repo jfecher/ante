2ff3239a8a757d9e5074e4fbb75c8fa74f94f6b0
polygonalbones <polybones@proton.me>
2026-05-01 21:31:22 -0400

    Update nix

--- flake.nix
+++ flake.nix
@@ -1,56 +1,117 @@
 {
+  description = "A safe, easy systems language";
+
   inputs = {
-    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nci = {
-      url = "github:yusdacra/nix-cargo-integration";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        parts.follows = "parts";
-      };
-    };
-    parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
+
+    crane.url = "github:ipetkov/crane";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ parts, nci, ... }:
-    parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs@{ self, nixpkgs, crane, rust-overlay }:
+    let
       systems = [
         "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
       ];
+      forAllSystems = function: nixpkgs.lib.genAttrs systems (system:
+        function (import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        }));
+    in
+      nixpkgs.lib.foldAttrs nixpkgs.lib.mergeAttrs {} (
+        nixpkgs.lib.mapAttrsToList (_: v: v) (
+          forAllSystems (pkgs:
+            let
+              system = pkgs.stdenv.hostPlatform.system;
 
-      imports = [
-        parts.flakeModules.easyOverlay
-        nci.flakeModule
-        ./crates.nix
-      ];
+              craneLib = (crane.mkLib pkgs).overrideToolchain (
+                p:
+                p.rust-bin.stable.latest.default
+              );
 
-      perSystem = { config, ... }:
-        let
-          anteCrate = config.nci.outputs.ante;
-          ante-lsCrate = config.nci.outputs.ante-ls;
-        in
-        {
-          overlayAttrs = {
-            inherit (config.packages)
-              ante
-              ante-ls;
-          };
-          packages = rec {
-            default = ante;
-            ante = anteCrate.packages.release;
-            ante-ls = ante-lsCrate.packages.release;
-          };
-          devShells.default = anteCrate.devShell.overrideAttrs (_: {
-            shellHook = ''
-              PATH=$PATH:$PWD/target/debug
-            '';
-          });
-        };
-    };
+              anteDeps = with pkgs; [
+                llvmPackages_21.llvm
+                libffi
+                libxml2
+                ncurses
+              ];
+
+              anteVersion = "0.1.0";
+            in {
+              packages.${system} =
+                let
+                  ante = craneLib.buildPackage {
+                    pname = "ante";
+                    version = anteVersion;
+
+                    src = pkgs.lib.fileset.toSource {
+                      root = ./.;
+                      fileset = pkgs.lib.fileset.unions [
+                        ./src
+                        ./Cargo.toml
+                        ./Cargo.lock
+                        ./build.rs
+                      ];
+                    };
+
+                    nativeBuildInputs = with pkgs; [
+                      installShellFiles
+                    ];
+
+                    buildInputs = anteDeps ++ [
+                      ante-stdlib
+                    ];
+
+                    postInstall = ''
+                      installShellCompletion --cmd ante \
+                        --bash <($out/bin/ante --shell-completion bash) \
+                        --fish <($out/bin/ante --shell-completion fish) \
+                        --zsh <($out/bin/ante --shell-completion zsh)
+                    '';
+
+                    /*postPatch = ''
+                      substituteInPlace tests/goldentests.rs --replace \
+                        'target/debug' "target/$(rustc -vV | sed -n 's|host: ||p')/release"
+                    '';*/
+                  };
+                  
+                  ante-ls = craneLib.buildPackage {
+                    pname = "ante-ls";
+                    version = "0.1.1";
+                    
+                    src = ./ante-ls;
+
+                    ANTE_STDLIB_DIR = "${ante-stdlib}/lib";
+                  };
+
+                  ante-stdlib = pkgs.stdenv.mkDerivation {
+                    pname = "ante-stdlib";
+                    version = anteVersion;
+
+                    src = ./stdlib;
+
+                    dontBuild = true;
+
+                    installPhase = ''
+                      mkdir -p $out/lib
+                      find . -type f -exec install -Dm644 "{}" -t $out/lib \;
+                    '';
+                  };
+                in {
+                  inherit ante ante-ls;
+                  default = ante;
+                };
+              
+              devShells.${system}.default = craneLib.devShell {
+                buildInputs = anteDeps;
+
+                LLVM_SYS_211_PREFIX = "${pkgs.llvmPackages_21.llvm.dev}";
+              };
+            })
+        ));
 }
